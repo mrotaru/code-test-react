cc7db05fd4f67c98ba1b2280377ba909b20e7995
Mihai Rotaru <mihai.rotaru@gmx.com>
2018-05-08 11:13:46 +0100

    checkout page

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -6,10 +6,27 @@ import Home from '../Home';
 import Checkout from '../Checkout';
 import Error from '../../components/Error'
 
-import * as beerActions from '../../modules/beers/actions'
-import BeersList from '../../components/BeersList'
+// const App = ({error}) => (
+//   <div>
+//     <header>
+//       <Link to="/">Home</Link>
+//       <Link to="/checkout">Checkout</Link>
+//     </header>
 
-const App = ({error}) => (
+//     <main>
+//       {error && <Error errorString={error} />}
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/checkout" component={Checkout} />
+//     </main>
+//   </div>
+// );
+
+// const mapStateToProps = state => ({
+//   error: state.beers.error,
+// })
+// export default connect(mapStateToProps, null)(App)
+
+const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
@@ -17,15 +34,10 @@ const App = ({error}) => (
     </header>
 
     <main>
-      {error && <Error errorString={error} />}
-      <Route exact path="/" component={Home} />
       <Route exact path="/checkout" component={Checkout} />
+      <Route exact path="/" component={Home} />
     </main>
   </div>
 );
 
-const mapStateToProps = state => ({
-  error: state.beers.error,
-})
-
-export default connect(mapStateToProps)(App)
+export default App
--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
-const Checkout = () => (
-  <div>
-    <h1>Checkout page</h1>
-  </div>
-);
+const Checkout = ({ items, count }) => {
+  const itemsArray = []
+  const inCart = new Map()
+  items.forEach(item => {
+    if (inCart.has(item.id)) {
+      const itemRef = inCart.get(item.id)
+      itemRef.count++
+    } else {
+      inCart.set(item.id, {...item, count: 1})
+    }
+  });
+  for(let value of inCart.values()) {
+    itemsArray.push(value)
+  }
+  return (
+    <div>
+      <h1>Checkout page</h1>
+      <ul>
+        {itemsArray.map(item => <li>{item.name} ( {item.count} )</li>)}
+      </ul>
+    </div>
+  )
+}
 
-export default Checkout;
+const mapStateToProps = state => ({
+  items: state.basket.items,
+  count: state.basket.count,
+})
+
+export default connect(mapStateToProps, null)(Checkout)

0ea09ea6aa1abf7fc5db290c6c754202886eb4c7
Mihai Rotaru <mihai.rotaru@gmx.com>
2018-05-08 10:12:52 +0100

    show '+' button on hover

--- src/components/Beer/index.js
+++ src/components/Beer/index.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
-const Beer = ({beer, addToCart, inCart}) => <div>
-    <span>{beer.name}</span>
-    {inCart && <span>( {inCart} )</span>}
-    <button onClick={() => {
-      addToCart(beer)
-    }}> + </button>
-  </div>
+class Beer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { showButton: false }
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+  }
+  onMouseEnter () {
+    this.setState({ showButton: true })
+  }
+  onMouseLeave () {
+    this.setState({ showButton: false })
+  }
+  render() {
+    const { beer, addToCart, inCart } = this.props
+    const { showButton } = this.state
+    return (
+      <div onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        <span>{beer.name}</span>
+        {inCart && <span>( {inCart} )</span>}
+        {showButton &&
+        <button onClick={() => {
+          addToCart(beer)
+        }}> + </button>}
+      </div>
+    )
+  }
+}
 
 export default Beer
